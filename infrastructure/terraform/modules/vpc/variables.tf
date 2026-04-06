718d4c1401d4b86fcddb8d57e44d811c25b2968b
h8815 <shimanshu8181@gmail.com>
2026-04-06 21:15:39 +0530

    feat: initialize Terraform infrastructure with VPC module and add project configuration files

--- infrastructure/terraform/modules/vpc/variables.tf
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -1,41 +1,29 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources into"
-  type        = string
-  default     = "eu-north-1"
-}
-
 variable "project_name" {
   description = "The name of the project, used for resource naming and tagging"
   type        = string
-  default     = "cybrmoon"
 }
 
 variable "environment" {
   description = "The deployment environment (e.g. production, staging, dev)"
   type        = string
-  default     = "production"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for the public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for the private subnets"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zones" {
   description = "List of availability zones to deploy subnets into"
   type        = list(string)
-  default     = ["eu-north-1a", "eu-north-1b"]
 }
